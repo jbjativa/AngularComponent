2b4b66b6099880d4a0329b2cfe2a3e332a8726f3
Jaime Jativa <jjativav@gmail.com>
2022-04-16 09:06:25 -0500

    ciclo de vida de componenetes

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,18 +1,40 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
 
   @Input() img: string = '';
   @Output() loaded= new EventEmitter<string>();
   imageDefault = "./assets/images/default2.png";
-  constructor() { }
+  constructor() {
+    //before render
+    //No async --once time
+    console.log('img constructor', 'imgValue:', this.img);
+
+  }
 
   ngOnInit(): void {
+    //before render
+    //async -fetch , requests, promises, etc --once time
+    console.log('img ngOnInit', 'imgValue:', this.img);
+  }
+  ngOnChanges() {
+    //before - during render
+    //change inputs --times
+    console.log('img ngOnChanges', 'imgValue:', this.img);
+  }
+  ngAfterViewInit(): void {
+    //after render -child render
+    // handler children
+    console.log('img ngAfterViewInit', 'imgValue:', this.img);
+  }
+  ngOnDestroy(): void {
+    // delete
+    console.log('img ngOnDestroy', 'imgValue:', this.img);
   }
 
   imgError()
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input} from '@angular/core';
+import {Product} from '../../models/product.model';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+
+  @Input('product') product!: Product;
 
   constructor() { }
 
